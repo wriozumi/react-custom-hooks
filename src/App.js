f3d8a7f4f5109e8e72fc3c179c25256dad9fe777
wriozumi <tshatmanaliev@gmail.com>
2020-09-27 23:07:56 +0600

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const useUserCollection = () => {
+  const [filter, setFilter] = React.useState("");
+  const [userCollection, setUserCollection] = React.useState([]);
+
+  const loadUsers = () => {
+    fetch(`https://jsonplaceholder.typicode.com/users?name_like=${filter}`)
+      .then(response => response.json())
+      .then(json => setUserCollection(json));
+  };
+
+  return { userCollection, loadUsers, filter, setFilter };
+};
+
+export const App = () => {
+  const { userCollection, loadUsers, filter, setFilter } = useUserCollection();
+
+  React.useEffect(() => {
+    loadUsers();
+  }, [filter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={filter} onChange={e => setFilter(e.target.value)} />
+      <ul>
+        {userCollection.map((user, index) => (
+          <li key={index}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
-}
-
-export default App;
+};
